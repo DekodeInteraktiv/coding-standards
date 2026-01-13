4bf345bd4deff0169a635fbc41ad84bb638bad72
Stian <stian.overasen@dekode.no>
2026-01-13 14:46:55 +0100

    Remove legacy eslint and stylelint packages

--- packages/eslint-plugin/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	configs: {
-		base: require( './lib/configs/base' ),
-		react: require( './lib/configs/react' ),
-	},
-};
--- packages/eslint-plugin/lib/configs/rules/strict.js
+++
@@ -1,7 +0,0 @@
-// https://eslint.org/docs/rules/#strict-mode
-
-module.exports = {
-	// Require or disallow strict mode directives
-	// https://eslint.org/docs/rules/strict
-	strict: [ 'error', 'never' ],
-};
